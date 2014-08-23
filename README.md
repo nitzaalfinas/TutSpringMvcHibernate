cc8bf930658d86792eb1295a80c8f35a38627d77
nitzaalfinas <nitzaalfinas@gmail.com>
2014-08-23 10:35:47 +0700

    modified:   README.md 	new file:   src/main/java/com/nitza/tuthibernate/model/Student.java 	new file:   target/classes/com/nitza/tuthibernate/model/Student.class

---
+++ README.md
@@ -0,0 +1,14 @@
+1. Setup dependency pada pom.xml
+    tambahkan 'spring-orm'
+    tambahkan 'hibernate-core' 4.3.5
+    tambahkan 'javax.servlet-api' 3.1.0
+    tambahkan 'javax.servlet.jsp-api' 2.3.1
+    tambahkan 'jstl' 1.2
+    tambahkan 'commons-dbcp2' 2.0
+    tambahkan 'mysql-connector-java' 5.1.30
+
+2. Setup database Student
+
+3. Tambahkan model Student
+
+4. Buat mapping xml untuk model Student
---
+++ src/main/java/com/nitza/tuthibernate/model/Student.java
@@ -0,0 +1,35 @@
+package com.nitza.tuthibernate.model;
+
+public class Student {
+	
+	private int studentId;
+	private String full_name;
+	private String address;
+	private String phone;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getFull_name() {
+		return full_name;
+	}
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+}
